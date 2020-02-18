a36823c4cbb8e73dfa0d5b33a48fc224f54f69ca
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-18 18:42:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,27 +11,27 @@ class Shoe
     @condition = new
   end
 
-  def color=(color)
-    @color=color
-  end
-  def color
-    @color
-  end
-  def size=(size)
-    @size = size
-  end
-  def size
-    @size
-  end
-  def material=(material)
-    @material = material
-  end
-  def material
-    @material
-  end
-  def condition=(condition)
-    @condition=condition
-  end
-  def condition
-  end
+  # def color=(color)
+  #   @color=color
+  # end
+  # def color
+  #   @color
+  # end
+  # def size=(size)
+  #   @size = size
+  # end
+  # def size
+  #   @size
+  # end
+  # def material=(material)
+  #   @material = material
+  # end
+  # def material
+  #   @material
+  # end
+  # def condition=(condition)
+  #   @condition=condition
+  # end
+  # def condition
+  # end
 end
