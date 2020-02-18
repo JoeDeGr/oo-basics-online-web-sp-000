80eade526afaccfdf03d6177e5c20a48a350c1bb
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-18 18:36:35 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,8 @@ class Book
   def initialize (title)
     @title = title
   end
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor  :author, :page_count, :genre
+  attr_reader :title
   # def title
   #   @title
   # end
